e6ed7c55bb8e51f84ec2e04492b7934116f6a9c7
Prince Ndu <princendu98@gmail.com>
2020-09-23 18:08:01 +0100

    updated version

--- src/components/cart/ShoppingCart.js
+++ src/components/cart/ShoppingCart.js
@@ -19,23 +19,24 @@ const ShoppingCart = (props) => {
         let amount = product.price * product.quantity
         total += amount
     }
-    const directions = total > 0 && User == "" ? <NavLink to="/login">
-        <button>Place Order</button></NavLink> : total > 0 && User != "" ? <NavLink to="/confirmOrder"><button>CONFIRM ORDER</button></NavLink> : ""
+    // const directions = total > 0 && User == "" ? <NavLink to="/login">
+    //     <button>Place Order</button></NavLink> : total > 0 && User != "" ? <NavLink to="/confirmOrder"><button>CONFIRM ORDER</button></NavLink> : ""
     const order = { "product": cart, total }
 
     return (
         <Fragment>
-            // {User.user != undefined && User.user != "" ? <div className="userNameDiv">
-                //     <p className="userName">Welcome, {`${User.user.first_name.toUpperCase()} ${User.user.last_name.toUpperCase()}`} </p>
-            // </div> : ""}
-            // <div className="orderList" >
-                //     {Ordered.length > 0 ? orderedlist : ""}
-            // </div>
-            <div className="orderListDisplay">
+             {User.user != undefined && User.user != "" ? <div className="userNameDiv">
+                     <p className="userName">Welcome, {`${User.user.first_name.toUpperCase()} ${User.user.last_name.toUpperCase()}`} </p>
+             </div> : ""}
+            <div className="orderList" >
+                    {Ordered.length > 0 ? orderedlist : ""}
+             </div>
+            <div className="orderListDisplay text-center mt-4">
                 <h3>CART</h3>
                 {itemDisplay}
                 < p className="amount">Total Amount: &#x20A6; {`${total}`}</p>
-                <p className="directions"> {directions}</p>
+                {/* <p className="directions"> {directions}</p> */}
+                <NavLink to="/confirmOrder"><button>CONFIRM ORDER</button></NavLink> 
             </div>
 
 
