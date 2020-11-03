a673750d225015ee46d042274e2ceea42275caf4
casdon_mystery <pascalemy2010@gmail.com>
2020-11-03 15:46:30 +0100

    build test

---
+++ build/precache-manifest.eed75d47b61a7e6dcda8f4ce58af15fa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00636dc28896a70f31bef3c53a86ccd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "719c4bbd702484917701",
+    "url": "/static/css/2.7444351b.chunk.css"
+  },
+  {
+    "revision": "a9f87b3d59033938069c",
+    "url": "/static/css/main.ab8ed446.chunk.css"
+  },
+  {
+    "revision": "719c4bbd702484917701",
+    "url": "/static/js/2.fb9a271d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fb9a271d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f87b3d59033938069c",
+    "url": "/static/js/main.ca34c6bb.chunk.js"
+  },
+  {
+    "revision": "d8aa5bfcc062b2f52ce0",
+    "url": "/static/js/runtime-main.8c374b5d.js"
+  },
+  {
+    "revision": "2d85a0e4ba39b4c186be7b7677bbb425",
+    "url": "/static/media/LAMELŌ logo blk.2d85a0e4.png"
+  },
+  {
+    "revision": "93cfddd9c2046f2b1efb107905023ada",
+    "url": "/static/media/LAMELŌ pattern blu2.93cfddd9.png"
+  },
+  {
+    "revision": "913cad5c30a7ed5a0472f6422f7bf959",
+    "url": "/static/media/Lamelo - Emblem (curves) wht.913cad5c.png"
+  },
+  {
+    "revision": "7438956bdc64004b1ad224b570111e5a",
+    "url": "/static/media/restaurant.7438956b.jpeg"
+  },
+  {
+    "revision": "3b9268cc43ff30e769a305ee6f72e46a",
+    "url": "/static/media/restaurant2.3b9268cc.jpg"
+  }
+]);
