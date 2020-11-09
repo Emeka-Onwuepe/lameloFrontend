c5da2f6767fae9e2bf25ccfc3e1c111dffa6260d
casdon_mystery <pascalemy2010@gmail.com>
2020-11-09 13:54:19 +0100

    added get sales features

--- src/pages/Sales/SalesList.js
+++ src/pages/Sales/SalesList.js
@@ -1,61 +1,70 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { ThemeContext } from '../Context/ThemeContext'
 import { Container, Card } from 'reactstrap';
-import { storeContext } from '../../components/State/State';
+import { storeContext, getSales } from '../../components/State/State';
 import './Sales.css';
 
 const SalesList = () => {
     const [sales, setSales] = useState([])
     const theme = useContext(ThemeContext);
-    const { storestate } = useContext(storeContext);
+    const { storestate, storedispatch } = useContext(storeContext);
     const { Orders, archive, logged } = storestate;
     const { isLightTheme, light, dark } = theme;
     const checkTheme = isLightTheme ? light : dark;
 
     let allSales = Orders.concat(archive);
 
+const{Sales}=storestate
 
     const dateNow = (date) => {
         const months = ['Jan', 'Feb', 'March', 'April', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         const dateYear = date.getFullYear();
         const dateMonth = date.getMonth();
         const dateDay = date.getDate().toString().length < 2 ? "0" + date.getDate().toString() : date.getDate();
-        const dateFormat = `${dateDay}/${months[dateMonth]}/${dateYear}` ;
+        const dateFormat = `${dateDay}/${months[dateMonth]}/${dateYear}`;
         return dateFormat;
     }
 
 
-    const getDaySales=(date, array)=>{
-       let filteredSales=[]
+    const getDaySales = (date, array) => {
+        let filteredSales = []
         array.forEach(sale => {
-            if(sale.created.indexOf(date)>-1){
+            if (sale.created.indexOf(date) > -1) {
                 filteredSales.push(sale)
             }
         });
         return filteredSales
     }
-    useEffect(() => {
-        setSales(getDaySales(dateNow(new Date()),allSales))
-    }, [])
 
-      if (!logged) {
+    useEffect(() => {
+        const sales = getDaySales(dateNow(new Date()), allSales)
+        setSales(sales)
+        let ids = []
+        sales.forEach(item => ids.push(item.id))
+        const config = { headers: { "Content-Type": "application/json", "Authorization": `Token ${storestate.User.token}` } }
+        const data = { "action": "Get_Sales", "data": ids, "customer": "", "search": "" }
+        getSales(data, config).then(res => storedispatch(res))
+
+    }, [])
+    console.log(Sales)
+    if (!logged) {
         return window.location = "/login";
     }
 
-    return (   
-       <Container className="dashboard-container">
+    return (
+        <Container className="dashboard-container">
             <Card style={{ backgroundColor: checkTheme.cardHeader, padding: '1rem' }}>
-                <h3 style={{textAlign: 'center', fontSize:'30px', backgroundColor: checkTheme.cardHeader}}>
+                <h3 style={{ textAlign: 'center', fontSize: '30px', backgroundColor: checkTheme.cardHeader }}>
                     Daily Sales
                 </h3>
                 <hr />
-               
+
                 {sales && sales.length > 0 ? <><h5 className="text-center">You have made</h5>
-                <h2 className="text-center sales-count">{sales.length}</h2>
+                    <h2 className="text-center sales-count">{sales.length}</h2>
                     <h5 className="text-center">{sales.length > 1 ? "Sales" : "Sale"}</h5>
                 </> : <p className="text-center">No Sales Yet</p>}
             </Card>
-        </Container> 
+        </Container>
 
     )
 }
